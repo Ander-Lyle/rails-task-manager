f55dcd7eb2e8ee10b96602bd3032e698c90c6ef5
James Chang <zhaoren@gmail.com>
2023-01-14 10:51:02 +0900

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -3,20 +3,4 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-
-  # Routes Syntax
-  # verb 'path', to: 'controller#action'
-
-  # 3 - As a user, I can list tasks
-  get 'tasks', to: 'tasks#list', as: 'tasks'
-  # 4 - As a user, I can view the details of a task
-  get 'tasks/new', to: 'tasks#new', as: 'new_task'
-  get 'tasks/:id', to: 'tasks#show', as: 'task'
-  # 5 - As a user, I can add a new task
-  post 'tasks', to: 'tasks#create'
-  # 6 - As a user, I can edit a task
-  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
-  patch 'tasks/:id', to: 'tasks#update'
-  # 7 - As a user, I can remove a task
-  delete 'tasks/:id', to: 'tasks#destroy'
 end
